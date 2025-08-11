e191ef04d3197c2b0c4517a61d214a4a69ef62f8
Sukanto Das <sukanto01899@gmail.com>
2025-08-12 01:20:39 +0600

    proxy set up

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,9 +8,9 @@ export default defineConfig({
   build: {
     outDir:'../server/dist'
   },
-  // server: {
-  //   proxy:{
-  //     '/api': 'http://localhost:3000'
-  //   }
-  // }
+  server: {
+    proxy:{
+      '/api': 'https://pingpong-live-chat-audio-video-call.onrender.com'
+    }
+  }
 })
