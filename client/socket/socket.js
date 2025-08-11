539e786aed2f5cd635e2e059b117cf8bc47ca3be
Sukanto Das <sukanto01899@gmail.com>
2025-08-12 01:32:05 +0600

    client set

--- client/socket/socket.js
+++ client/socket/socket.js
@@ -3,9 +3,8 @@ import io from 'socket.io-client';
 let socket = null
 
 export const connectSocket = ()=>{
-    socket = io({
+    socket = io('https://pingpong-live-chat-audio-video-call.onrender.com',{
         withCredentials: true,
-        transports: ['websocket']
     })
 
     return socket;
